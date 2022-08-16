421c76d35d8414978e0a6b547824428ec4207b35
Trey Bach <treybachyt@gmail.com>
2022-08-16 10:29:47 -0700

    Commit #2
    Fixed Scripts and Added HTML Elements,
    added for loop to add for search

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,13 @@
 
 var apiKey = "20667418638774cf5af9135791e28d78"
+var units = "imperial"
 var cityLocation = "london";
 var requestURL = "https:api.openweathermap.org/data/2.5/weather?q=" + cityLocation + "&appid=" + apiKey;
 
-searchBtn = $("#searchButton")
+searchBtn = document.getElementById("searchButton")
+searchForecastContainer = document.getElementById("searchForecastContainer")
+
+
 
 function getCity(city){
     var geocodeUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`
@@ -22,7 +26,7 @@ function getCity(city){
     }
   
       function getWeather(lat, lon){
-        var weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`
+        var weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${units}`
   
         fetch(weatherUrl)
         .then(function (response) {
@@ -32,13 +36,60 @@ function getCity(city){
         })
         .then(function (data) {
           console.log(data);
+
+
+          // displays the date, an icon representation of weather conditions, the temperature, the wind speed, the humidity
+          for (let i = 1; i < 6; i++){  
+    
+            var searchResultDate = document.createElement("ul")
+            searchResultDate.innerText = "Date"
+
+            var test = 79
+
+            var searchResultIcon = document.createElement("ul")
+            if(test >= 80){
+              searchResultIcon.innerText = "good"
+            } else if (test < 80 && test > 70){
+              searchResultIcon.innerText = "aight"
+            } else {
+              searchResultIcon.innerText = "could be better"
+            }
+
+            var searchResultTemp = document.createElement("ul")
+            searchResultTemp.innerText = test
+
+            var searchResultWind = document.createElement("ul")
+            searchResultWind.innerText = data.wind.speed + " mph"
+
+            var searchResultHumidity = document.createElement("ul")
+            searchResultHumidity.innerText = data.main.humidity + "%"
+
+            var searchResultCard = document.createElement("div")
+
+
+            searchResultCard.classList.add("card")
+            searchResultCard.classList.add("bg-dark")
+            searchResultCard.classList.add("mx-2")
+            searchResultCard.classList.add("text-white")
+
+            searchForecastContainer.append(searchResultCard)
+            searchResultCard.appendChild(searchResultDate)
+            searchResultCard.appendChild(searchResultIcon)
+            searchResultCard.appendChild(searchResultTemp)
+            searchResultCard.appendChild(searchResultWind)
+            searchResultCard.appendChild(searchResultHumidity)
+            
+          }
+
+
         });
       }
-  
-      searchBtn.on("click", function(){
-        var cityInput = $(this).siblings("#searchSearchInput").val();
-  
-        getCity(cityInput);
-      });
+
+      searchBtn.addEventListener("click", function(){
+        searchInput = document.getElementById("citySearchInput").value;
+        console.log(searchInput)
+        getCity(searchInput);
+      })
+
 
 
